3d57cdff9fa529eec3dc44dbc0098b5096871ce9
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 18:54:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,6 @@ katz_deli = []
 def take_a_number(array, name)
   array << "name"
   array.each_with_index do |i, index|
-    if array.length == 1 
-      puts "Welcome, #{name}. You are number 1 in line."
-     else array.length > 1
       puts "Welcome, #{name}. You are number #{index+1} in line."
   end
 end
