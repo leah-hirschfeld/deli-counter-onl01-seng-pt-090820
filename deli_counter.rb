228c21dba6b2d3f0f7c6a1ce3367992e475c204a
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 18:35:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(array, name)
+  array << #{name}
+  if array.length == 0 
+    puts "There is nobody in line"
+  else array.length > 0
+    puts "There are already people in line"
+    array.each_with_index do |i, index|
+    puts "Welcome, #{name}. You are number #{index + 1} in line."
+  end
+end
+end
