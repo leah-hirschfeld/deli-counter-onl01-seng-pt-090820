9cfb268f0f60b2657007effa3aa9a166fd16c8b8
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 18:57:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def take_a_number(array, name)
   array = []
-  array << "name"
   array.each_with_index do |i, index|
+      array << "name"
       puts "Welcome, #{name}. You are number #{index+1} in line."
   end
 end
