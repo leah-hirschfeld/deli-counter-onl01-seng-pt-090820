7c2195ed796eac43596eb849a9b2d05c6afb3dc5
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 20:23:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(array, name)
     puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving(array, name)
+def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   else
