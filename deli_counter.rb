96167d9d224019f8473b099d69ecd949a3e4699d
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 20:25:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   else
-    name = array.pop 
+    name = array.shift 
     array
     puts "Currently serving #{name}."
   end
