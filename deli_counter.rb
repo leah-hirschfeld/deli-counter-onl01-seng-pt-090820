53fe5e5c156b12e684949500f5b9e71d5277b40f
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 19:48:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,10 +12,11 @@ def line(array)
   end
 end   
 
-def take_a_number(array, name)
-  array.each_with_index do |i, index|
-      array << name
-      puts "Welcome, #{name}. You are number #{index+1} in line."
+def take_a_number(array)
+  if array.length = 0 
+    puts "There is nobody in line."
+  else 
+    puts "Welcome, #{name}. You are number #{index} in line"
   end
 end
 
