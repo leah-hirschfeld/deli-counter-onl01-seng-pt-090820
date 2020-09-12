e7bbe925d42a46e32f137bf274df84543c37c171
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 19:10:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 def line(array)
   array.each_with_index do |i, index|
     if array.length == 1 
-      puts "The line is empty. "
+      puts "The line is empty."
     else 
       puts "The line is currently #{index+1}. #{i}"
   end
@@ -12,7 +12,7 @@ end
 
 def take_a_number(array, name)
   array.each_with_index do |i, index|
-      array << "name"
+      array << name
       puts "Welcome, #{name}. You are number #{index+1} in line."
   end
 end
