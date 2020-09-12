ca3359d71af6ecfb52cf16c8c9a0626d647ee31d
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 19:08:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,10 +7,12 @@ def line(array)
     else 
       puts "The line is currently #{index+1}. #{i}"
   end
+  end
 end    
 
 def take_a_number(array, name)
   array.each_with_index do |i, index|
       array << "name"
       puts "Welcome, #{name}. You are number #{index+1} in line."
+  end
 end
