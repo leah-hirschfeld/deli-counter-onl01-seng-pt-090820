fd9f3e290c01dfadb99f9e3af3a0e779bf330e61
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 19:43:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,16 @@
   array = []
 
 def line(array)
-    if array.length == 0
+  if array.length == 0
       puts "The line is currently empty."
     else 
-      puts "The line is currently:" 
+      string = "The line is currently: "  
       array.each_with_index do |i, index|
-      puts "#{index+1}. #{i}"
+      string += " #{index+1}. #{i}"
   end
+  puts string
   end
-end  
+end   
 
 def take_a_number(array, name)
   array.each_with_index do |i, index|
